63f9a211df052f6e5dd3d9002eb9e320264de74b
kortsa <leoklomegah8@gmail.com>
2024-03-07 17:16:43 -0500

    writing a todo list with ReactJs

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import Todo from "./components/Todo";
+import Form from "./components/form";
+import FilteredButton from "./components/filterButton";
+
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+    console.log(updatedTasks);
+  }  
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  
+
+  const taskList = tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name, completed: false };
+
+    setTasks([...tasks, newTask]);
+  }
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={ addTask } />
+      <div className="filters btn-group stack-exception">
+        <FilteredButton name="All" />
+        <FilteredButton name="Active" />
+        <FilteredButton name="Completed" />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/filterButton.jsx
@@ -0,0 +1,13 @@
+function FilteredButton(props) {
+    return (
+        <div>
+            <button type="button" className="btn toggle-btn" aria-pressed="true">
+                <span className="visually-hidden">Show </span>
+                <span>{props.name}</span>
+                <span className="visually-hidden"> tasks</span>
+            </button>
+        </div>
+    )
+}
+
+export default FilteredButton;
